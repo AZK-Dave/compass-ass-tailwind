6a7a3b2700e8f681feef1ffebd23d94cc5594acd
Dave <115076680+AZK-Dave@users.noreply.github.com>
2023-04-02 14:01:36 +0630

    Add files via upload

---
+++ Tailwind/src/App.jsx
@@ -0,0 +1,132 @@
+
+import './App.css'
+
+
+function App() {
+
+  return (
+    <>
+      <div className='flex flex-col items-center mt-10'>
+
+      <div className='my-5 text-center font-bold text-2xl'>Your Choice Plans</div>
+
+      <p className='text-center max-w-md text-xs '>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia praesentium sapiente officia iure. Saepe dolorem vel unde. Ullam, mollitia amet totam fugit suscipit libero esse dolor. Veniam aperiam velit quaerat?</p>
+
+        <div className='gap-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 2xl:grid-cols-4'>
+          
+        <div className='w-60 h-96  pl-5 order-2 lg:order-1 2xl:order-1  hover:shadow-lg hover:shadow-[#E87070] hover:border-[#E87070] border-2 border-[#fda3a3] container my-8 rounded-xl mx-auto'>
+            <div className='text-[#E87070] pt-6 text-xs'>Start</div>
+            <div className='text-[#E87070] mt-3 text-3xl'>Free</div>
+            
+
+            <div className='my-9'>
+              
+              <p className='text-base'>
+                <p className='inline text-[#E87070] pr-1'>&#10003;</p>
+                content1</p>
+              <p className='text-base'>
+              <p className='inline text-[#E87070] pr-1'>&#10003;</p>  content1</p>
+              <p className='text-base'>
+                <p className='inline text-[#E87070] pr-1'>&#10003;</p>
+                content1</p>
+              <p className='text-base'>
+              <p className='inline text-[#E87070] pr-1'>&#10003;</p>
+                content1</p>
+              <p className='text-base'>
+              <p className='inline text-[#E87070] pr-1'>&#10003;</p>
+                content1</p>
+            </div>
+              <button className='hover:bg-[#E87070] hover:text-white text-[#E87070] bg-white rounded-md border-2 border-[#fda3a3] w-48 h-9 text-base'>Buy</button>
+              <p className='mt-3 text-xs'>Lorem ipsum dolor sit amet consectetur adipisicing </p>
+          </div>
+
+        <div className='w-60 h-96 pl-5  0rder-1 lg:order-2 2xl:order-2 hover:shadow-lg hover:shadow-[#E87070] hover:border-[#E87070] border-2 border-[#fda3a3] container my-8 rounded-xl mx-auto'>
+            <div className='text-[#E87070] pt-6 text-xs'>Start</div>
+            <div className='text-[#E87070] mt-3 text-3xl'>Best</div>
+            
+
+            <div className='my-9'>
+              
+              <p className='text-base'>
+                <p className='inline text-[#E87070] pr-1'>&#10003;</p>
+                content1</p>
+              <p className='text-base'>
+              <p className='inline text-[#E87070] pr-1'>&#10003;</p>  content1</p>
+              <p className='text-base'>
+                <p className='inline text-[#E87070] pr-1'>&#10003;</p>
+                content1</p>
+              <p className='text-base'>
+              <p className='inline text-[#E87070] pr-1'>&#10003;</p>
+                content1</p>
+              <p className='text-base'>
+              <p className='inline text-[#E87070] pr-1'>&#10003;</p>
+                content1</p>
+            </div>
+              <button className='hover:bg-[#E87070] hover:text-white text-[#E87070] bg-white border-2 border-[#fda3a3] rounded-md w-48 h-9 text-base'>Buy</button>
+              <p className='mt-3 text-xs'>Lorem ipsum dolor sit amet consectetur adipisicing </p>
+          </div>
+
+
+        <div className='w-60 h-96 pl-5  order-3 hover:shadow-lg hover:shadow-[#E87070] hover:border-[#E87070] border-2 border-[#fda3a3] container my-8 rounded-xl mx-auto'>
+            <div className='text-[#E87070] pt-6 text-xs'>Start</div>
+            <div className='text-[#E87070] mt-3 text-3xl'>Medium</div>
+            
+
+            <div className='my-9'>
+              
+              <p className='text-base'>
+                <p className='inline text-[#E87070] pr-1'>&#10003;</p>
+                content1</p>
+              <p className='text-base'>
+              <p className='inline text-[#E87070] pr-1'>&#10003;</p>  content1</p>
+              <p className='text-base'>
+                <p className='inline text-[#E87070] pr-1'>&#10003;</p>
+                content1</p>
+              <p className='text-base'>
+              <p className='inline text-[#E87070] pr-1'>&#10003;</p>
+                content1</p>
+              <p className='text-base'>
+              <p className='inline text-[#E87070] pr-1'>&#10003;</p>
+                content1</p>
+            </div>
+              <button className='hover:bg-[#E87070] hover:text-white text-[#E87070] bg-white border-2 border-[#fda3a3] rounded-md w-48 h-9 text-base'>Buy</button>
+              <p className='mt-3 text-xs'>Lorem ipsum dolor sit amet consectetur adipisicing </p>
+          </div>
+
+
+        <div className='w-60 h-96 pl-5  order-4 hover:shadow-lg hover:shadow-[#E87070] hover:border-[#E87070] border-2 border-[#fda3a3] container my-8 rounded-xl mx-auto'>
+            <div className='text-[#E87070] pt-6 text-xs'>Start</div>
+            <div className='text-[#E87070] mt-3 text-3xl'>Fair</div>
+            
+
+            <div className='my-9'>
+              
+              <p className='text-base'>
+                <p className='inline text-[#E87070] pr-1'>&#10003;</p>
+                content1</p>
+              <p className='text-base'>
+              <p className='inline text-[#E87070] pr-1'>&#10003;</p>  content1</p>
+              <p className='text-base'>
+                <p className='inline text-[#E87070] pr-1'>&#10003;</p>
+                content1</p>
+              <p className='text-base'>
+              <p className='inline text-[#E87070] pr-1'>&#10003;</p>
+                content1</p>
+              <p className='text-base'>
+              <p className='inline text-[#E87070] pr-1'>&#10003;</p>
+                content1</p>
+            </div>
+              <button className='hover:bg-[#E87070] hover:text-white text-[#E87070] bg-white border-2 border-[#fda3a3] rounded-md w-48 h-9 text-base'>Buy</button>
+              <p className='mt-3 text-xs'>Lorem ipsum dolor sit amet consectetur adipisicing </p>
+          </div>
+
+ 
+          
+          
+        </div>
+        </div>
+    </>
+  )
+}
+
+export default App
